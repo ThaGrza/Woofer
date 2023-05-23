b086f7e1b501b21e8ee8ce505a050d74db6c8fcf
ThaGrza <andrewgriswold7@gmail.com>
2023-05-22 21:51:34 -0400

    google place api route

--- backend/routes/googleParks.js
+++ backend/routes/googleParks.js
@@ -1,34 +1,22 @@
-const Axios = require('axios');
+const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
 
 
-
-
-
-let config = {
-    method: 'get',
-    url: "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Dog%20Parks&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=" + process.env.REACT_APP_API_KEY,
-    headers: { }
-};
-
-router.post('/parkLocation', (req,res) => {
-  return res.send(req)
-  // console.log(req);
-  // console.log(res);
-  // Axios.request(config)
-  // .then(res => {
-  //   // console.log(res.data);
-  // }).catch(err => {
-  //   console.log("Error: " + err);
-  //   console.log("Google Api Error: " + err.response.data.error);
-  // })
-});
-
-
-router.get('/getParks', (req,res) => {
-    return res.status(200).send({message: 'Success hitting /getParks'});
+router.get('/getParks/:location', async (req,res) => {
+  try{
+    let location = req.params.location;
+    let apiKey = process.env.REACT_APP_API_KEY;
+    let apiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=dog+parks+in+${location}&key=${apiKey}`
+    const response = await axios.get(apiUrl);
+    const parks = response.data;
+    console.log(parks);
+    return res.status(200).json(parks);
+  } catch(err){
+    console.log(err);
+    res.status(500).json({Error: 'Something Blew UP =('})
+  }
 });
 
 module.exports = router;
